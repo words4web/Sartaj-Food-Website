87d2f5abead7024b0c5253e44ef287d9833d4caa
mazahir7 <mazahir1220@gmail.com>
2026-08-20 13:37:30 +0530

    feat: integrate Google Tag Manager and correct TypeScript route type reference path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,15 @@ export default function RootLayout({
     <html lang="en" className={`bg-background ${playfair.variable}`}>
       <head />
       <body className="font-sans antialiased relative min-h-screen">
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PVDFQRH7');
+          `}
+        </Script>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-88KYPVV2L3"
           strategy="afterInteractive"
@@ -67,6 +76,14 @@ export default function RootLayout({
             gtag('config', 'AW-375874196');
           `}
         </Script>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PVDFQRH7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {isMaintenance ? (
           <MaintenancePage />
         ) : (
